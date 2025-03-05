f0c5cb1da76e578f1d8644784bd4c564866ff8c6
Alexander Mueller <2320040+alexandermueller@users.noreply.github.com>
2025-03-05 04:42:53 -0500

    Rename `GameList.gameList` To `list`

--- WhatToPlay/game_list.py
+++ WhatToPlay/game_list.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 class GameList:
 	def __init__(self, gameList: Optional[str] = None):
-		self.gameList = [game.strip() for game in gameList.split('\n')] if gameList else []
+		self.list = [game.strip() for game in gameList.split('\n')] if gameList else []
 
 	def description(self) -> str:
-		return '\n'.join(self.gameList) if self.gameList else ''
+		return '\n'.join(self.list) if self.list else ''
